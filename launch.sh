0c338dc03242e279696a55cbdc9fade4339306c1
Koby Lewis <kobyjlewis@gmail.com>
2025-10-28 13:37:05 -0600

    add main.py and launch.sh

---
+++ launch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH --time=01:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --gpus=1
+#SBATCH --mem-per-cpu=128G   # memory per CPU core
+#SBATCH --output=slurm_logs/%j.out
+
+python src/main.py fit
