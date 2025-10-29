23e5a543a1bd1e81ee5432df3a8bfc765a7305cb
Koby Lewis <kobyjlewis@gmail.com>
2025-10-29 09:54:34 -0600

    make sure placeholder is minimally viable to work with the fit method

--- src/models/placeholder_model.py
+++ src/models/placeholder_model.py
@@ -1,11 +1,16 @@
 import torch
 import torch.optim as optim
+import torch.nn as nn
 from models.mirror_model import MirrorModel
 
 
 class PlaceholderModel(MirrorModel):
+    def __init__(self) -> None:
+        super().__init__()
+        self.parameter = nn.Parameter(torch.tensor(0.0))
+
     def training_step(self, tokens, attention_mask):
-        return torch.tensor(0.0)
+        return self.parameter
 
     def configure_optimizers(self):
-        return optim.AdamW([])
+        return optim.AdamW(self.parameters())
