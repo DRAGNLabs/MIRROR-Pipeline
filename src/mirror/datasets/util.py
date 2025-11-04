53eae0a6ee81770969247c8618c742faee1103f8
Koby Lewis <kobyjlewis@gmail.com>
2025-11-04 09:10:32 -0700

    add clarifying comment and looser type

--- src/mirror/datasets/util.py
+++ src/mirror/datasets/util.py
@@ -11,9 +11,16 @@ datasets_path = mirror_data_path / 'datasets'
 def load_hf_from_cache_or_download(
         hf_dataset_path: str,
         hf_dataset_name: str | None = None,
-        process: Callable[[Dataset | DatasetDict], Dataset] | None = None,
+        process: Callable[[Dataset | DatasetDict], Dataset | DatasetDict] | None = None,
         use_cache: bool = True
 ) -> Dataset | DatasetDict:
+    """
+    The first time this is called with a particular path/name pair, it will download
+    the dataset from huggingface and apply the process function to it. Thereafter,
+    if it is called again with use_cache=True, it will used the cached data from the
+    first run. Note that if you have changed your process method, you'll need to set
+    use_cache=False to run the new processing.
+    """
     dataset_path = datasets_path / hf_dataset_path / hf_dataset_name \
         if hf_dataset_name else datasets_path / hf_dataset_path
     is_cached = use_cache and os.path.exists(dataset_path)
