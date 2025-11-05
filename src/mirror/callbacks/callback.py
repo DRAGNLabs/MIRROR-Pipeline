dba304992b16efa8118f0e12e7915206a774fe0d
Koby Lewis <kobyjlewis@gmail.com>
2025-11-05 15:20:43 -0700

    add import

--- src/mirror/callbacks/callback.py
+++ src/mirror/callbacks/callback.py
@@ -1,4 +1,5 @@
 from lightning import Fabric
+from torch.optim import Optimizer
 
 from mirror.models.mirror_model import MirrorModel
 from mirror.types import TokenBatch, AttentionMaskBatch, Loss
@@ -10,10 +11,10 @@ class Callback:
     Callback class: https://lightning.ai/docs/pytorch/stable/api/lightning.pytorch.callbacks.Callback.html#lightning.pytorch.callbacks.Callback
     """
 
-    def on_fit_start(self, fabric: Fabric, model: MirrorModel):
+    def on_fit_start(self, fabric: Fabric, model: MirrorModel, optimizer: Optimizer, training_run_id: str):
         pass
 
-    def on_fit_end(self, fabric: Fabric, model: MirrorModel):
+    def on_fit_end(self, fabric: Fabric, model: MirrorModel, optimizer: Optimizer, training_run_id: str):
         pass
 
     def on_train_batch_end(self, fabric: Fabric, model: MirrorModel, loss: Loss, tokens: TokenBatch, attention_mask: AttentionMaskBatch, batch_idx: int):
