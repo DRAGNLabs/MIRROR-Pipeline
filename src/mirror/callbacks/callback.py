cd70f460ad93ee22c6dc15cb6b3862c13369268b
Koby Lewis <kobyjlewis@gmail.com>
2025-11-05 15:18:08 -0700

    add on_fit_start and on_fit_end

--- src/mirror/callbacks/callback.py
+++ src/mirror/callbacks/callback.py
@@ -5,5 +5,16 @@ from mirror.types import TokenBatch, AttentionMaskBatch, Loss
 
 
 class Callback:
+    """
+    The names of the methods here are based on those of Lightning's 
+    Callback class: https://lightning.ai/docs/pytorch/stable/api/lightning.pytorch.callbacks.Callback.html#lightning.pytorch.callbacks.Callback
+    """
+
+    def on_fit_start(self, fabric: Fabric, model: MirrorModel):
+        pass
+
+    def on_fit_end(self, fabric: Fabric, model: MirrorModel):
+        pass
+
     def on_train_batch_end(self, fabric: Fabric, model: MirrorModel, loss: Loss, tokens: TokenBatch, attention_mask: AttentionMaskBatch, batch_idx: int):
         pass
--- src/mirror/trainer.py
+++ src/mirror/trainer.py
@@ -4,6 +4,7 @@ from typing import List
 import datetime
 
 from mirror.callbacks.callback import Callback
+from mirror.callbacks.checkpoint_callback import CheckpointCallback
 from mirror.datasets.mirror_dataset import MirrorDataset
 from mirror.datasets.preprocessed_dataset import PreprocessedDataset
 from mirror.models.mirror_model import MirrorModel
@@ -11,7 +12,9 @@ from mirror.models.mirror_model import MirrorModel
 
 class Trainer:
     def __init__(self, callbacks: List[Callback] = []) -> None:
-        default_callbacks = []
+        default_callbacks = [
+            CheckpointCallback()
+        ]
 
         callbacks = [*default_callbacks, *callbacks]
         self.fabric = Fabric(callbacks=callbacks)
@@ -25,6 +28,8 @@ class Trainer:
         dataloader = DataLoader(preprocessed_dataset)
         dataloader = self.fabric.setup_dataloaders(dataloader)
 
+        self.fabric.call('on_fit_start', fabric=self.fabric, model=model, optimizer=optimizer, training_run_id=training_run_id)
+
         for batch_idx, (tokens, attention_mask) in enumerate(dataloader):
             optimizer.zero_grad()
             loss = model.training_step(tokens, attention_mask)
@@ -32,3 +37,5 @@ class Trainer:
             optimizer.step()
 
             self.fabric.call('on_train_batch_end', fabric=self.fabric, model=model, loss=loss, tokens=tokens, attention_mask=attention_mask, batch_idx=batch_idx)
+
+        self.fabric.call('on_fit_end', fabric=self.fabric, model=model, optimizer=optimizer, training_run_id=training_run_id)
