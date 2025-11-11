89dc4d5e6933a90d435b7e4590b504f098b84118
Koby Lewis <kobyjlewis@gmail.com>
2025-11-11 16:19:20 -0700

    supress warnings

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from jsonargparse import auto_parser
 from typing import List, Literal
+import warnings
 
 from lightning.fabric.strategies.strategy import Strategy
 from lightning.fabric.strategies.fsdp import FSDPStrategy
@@ -23,6 +24,12 @@ def main(
     callbacks: List[Callback] = [],
     checkpoint: CheckpointIdentifier | None = None,
 ):
+    # These warnings happen internal to Fabric, so there's not much we can do about them.
+    warnings.filterwarnings('ignore', category=FutureWarning, message='.*Please use DTensor instead and we are deprecating ShardedTensor.*')
+    warnings.filterwarnings('ignore', category=FutureWarning, message='.*`load_state_dict` is deprecated and will be removed in future versions\\. Please use `load` instead.*')
+    warnings.filterwarnings('ignore', category=UserWarning, message='.*Please use the new API settings to control TF32 behavior.*')
+    warnings.filterwarnings('ignore', category=UserWarning, message='.*`_get_pg_default_device` will be deprecated, it only stays for backward-compatibility reason.*')
+
     match subcommand:
         case 'fit':
             fit(data, strategy, devices, num_nodes, callbacks, checkpoint)
