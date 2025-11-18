4db6d51b0c5201b550c58b11c36b9f207d0b7ee3
Koby Lewis <kobyjlewis@gmail.com>
2025-11-18 11:39:58 -0700

    Merge pull request #38 from DRAGNLabs/4-strategies-distributed-training
    4 strategies distributed training

--- launch.sh
+++ launch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH --time=01:00:00   # walltime
-#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --ntasks-per-node=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
-#SBATCH --gpus=1
+#SBATCH --gpus-per-node=1
 #SBATCH --mem-per-cpu=128G   # memory per CPU core
 #SBATCH --output=slurm_logs/%j.out
 
 source env/bin/activate
 
-python src/main.py fit --data.class_path WikitextDataset --data.head 10
+srun python src/main.py fit --data.class_path WikitextDataset --data.head 10
