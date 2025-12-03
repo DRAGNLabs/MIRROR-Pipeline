27e99a18eda9a96ccccf10f87b933574b71a476a
logicalpencils <82343982+logicalpencils@users.noreply.github.com>
2025-12-03 15:59:28 -0700

    main.py builds and runs jinja script

--- src/main.py
+++ src/main.py
@@ -17,6 +17,7 @@ import lightning.fabric.strategies
 import mirror.datasets
 
 from jinja2 import Environment, FileSystemLoader
+import subprocess
 
 Subcommand = Literal['fit'] | Literal['test']
 
@@ -37,38 +38,18 @@ def main(
     checkpoint: CheckpointIdentifier | None = None,
     
 ):
-    # These warnings happen internal to Fabric, so there's not much we can do about them.
-    warnings.filterwarnings('ignore', category=FutureWarning, message='.*Please use DTensor instead and we are deprecating ShardedTensor.*')
-    warnings.filterwarnings('ignore', category=FutureWarning, message='.*`load_state_dict` is deprecated and will be removed in future versions\\. Please use `load` instead.*')
-    warnings.filterwarnings('ignore', category=UserWarning, message='.*Please use the new API settings to control TF32 behavior.*')
-    warnings.filterwarnings('ignore', category=UserWarning, message='.*`_get_pg_default_device` will be deprecated, it only stays for backward-compatibility reason.*')
+    try:
+        result = subprocess.run(
+            ["sbatch"],
+            input=slurm_job.encode('utf-8'), # Encode the string to bytes
+            capture_output=True,
+            text=True,
+            check=True
+        )
+    except subprocess.CalledProcessError as e:
+        print(f"Error submitting SLURM job: {e}")
+        print(f"Stderr: {e.stderr}")
 
-    
-    
-    match subcommand:
-        case 'fit':
-            fit(data, strategy, devices, num_nodes, callbacks, checkpoint)
-        case _:
-            print(f'unimplemented subcommand: {subcommand}')
-
-
-def fit(
-    dataset: MirrorDataset,
-    strategy: Strategy,
-    devices: int,
-    num_nodes: int,
-    callbacks: List[Callback],
-    checkpoint: CheckpointIdentifier | None
-):
-    trainer = Trainer(strategy, devices, num_nodes, callbacks)
-
-    trainer.launch()
-
-    with trainer.fabric.init_module():
-        model = PlaceholderModel()
-
-    trainer.fit(model, dataset, checkpoint)
-    
 
 if __name__ == '__main__':
     env = Environment(loader = FileSystemLoader('templates'))
