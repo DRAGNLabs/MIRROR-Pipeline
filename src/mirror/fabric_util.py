60b27d31ff5c81ccfd399b105b3bc630f16b3ef4
Koby Lewis <kobyjlewis@gmail.com>
2025-12-05 15:35:55 -0700

    add clarifying comment

--- src/mirror/fabric_util.py
+++ src/mirror/fabric_util.py
@@ -4,4 +4,6 @@ from lightning import Fabric
 
 def rank_zero_log(fabric: Fabric, *args):
     if fabric.is_global_zero:
+        # stderr because by default python buffers stdout, which can be confusing for debugging.
+        # a lot of libraries seem to use this strategy of printing to stderr instead
         print(*args, file=sys.stderr)
