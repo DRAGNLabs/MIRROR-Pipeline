da086ea4a51924d4e9cea2b2e926f544531ea08c
Jansen Nye <jansen05@login01.rc.byu.edu>
2026-01-14 15:15:42 -0700

    Revert accidentally deleting whole function instead of print statement

--- src/mirror/util.py
+++ src/mirror/util.py
@@ -23,3 +23,19 @@ def assert_can_download(item_name_to_download: str):
 
 def is_power_of_ten(n: int):
     return n > 0 and math.log10(n).is_integer()
+
+def pad_to_longest(batch: list[TokenTensor], pad_token: int):
+    lens = torch.tensor([b.numel() for b in batch], device=device, dtype=torch.long)
+    max_len = lens.max().item()
+    batch_size = len(batch)
+
+    tokens = torch.full((batch_size, max_len), int(pad_token), dtype=torch.long, device=device)
+
+    ar = torch.arange(max_len, device=device)
+    attention_mask = (ar.unsqueeze(0) < lens.unsqueeze(1)).to(torch.long)
+
+    for i, b in enumerate(batch):
+        L = b.numel()
+        tokens[i,:L] = b
+    
+    return tokens, attention_mask
