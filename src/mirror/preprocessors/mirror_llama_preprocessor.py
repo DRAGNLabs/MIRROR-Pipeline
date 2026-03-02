7b76e4eb3aaef8c3efb248ed57bdf89efb80a26b
Jansen Nye <jansen05@login03.rc.byu.edu>
2026-03-02 16:22:21 -0700

    Remove encode methods/pad_token_id, add MirrorProcessor type args

--- src/mirror/preprocessors/mirror_llama_preprocessor.py
+++ src/mirror/preprocessors/mirror_llama_preprocessor.py
@@ -7,22 +7,21 @@ from mirror.types import TokenTensor, TokenBatch, AttentionMaskBatch
 from mirror.util import get_device, pad_to_longest
 from mirror.row_types import TextRow
 
-class MirrorLlamaPreprocessor(MirrorPreprocessor):
+class MirrorLlamaPreprocessor(
+    MirrorPreprocessor[TextRow, TokenTensor, tuple[TokenBatch, AttentionMaskBatch]]
+):
     def __init__(self, hf_model_name: str) -> None:
         self._hf_model_name = hf_model_name
         self._tokenizer: PreTrainedTokenizerBase = load_hf_tokenizer(hf_model_name)
         if self._tokenizer.pad_token_id is None:
             self._tokenizer.pad_token = self._tokenizer.eos_token
 
-    def encode(self, text: str) -> TokenTensor:
-        ids = self._tokenizer.encode(text, add_special_tokens=True)
+    def preprocess_example(self, example: TextRow) -> TokenTensor:
+        ids = self._tokenizer.encode(example['text'], add_special_tokens=True)
         if len(ids) < 2:
             eos = self._tokenizer.eos_token_id
             ids = [eos, eos] if len(ids) == 0 else [*ids, eos]
         return torch.tensor(ids, device=get_device(), dtype=torch.long)
-
-    def preprocess_example(self, example: TextRow) -> TokenTensor:
-        return self.encode(example['text'])
 
     def collate(self, examples: list[TokenTensor]) -> tuple[TokenBatch, AttentionMaskBatch]:
         return pad_to_longest(examples, pad_token=self.pad_token_id)
--- src/mirror/preprocessors/placeholder_preprocessor.py
+++ src/mirror/preprocessors/placeholder_preprocessor.py
@@ -5,10 +5,9 @@ from mirror.util import get_device, pad_to_longest
 from mirror.row_types import TextRow
 from mirror.types import TokenTensor, TokenBatch, AttentionMaskBatch
 
-class PlaceholderPreprocessor(MirrorPreprocessor):
-    def encode(self, text):
-        return torch.tensor([1, 2, 3, 4], device=get_device())
-
+class PlaceholderPreprocessor(
+    MirrorPreprocessor[TextRow, TokenTensor, tuple[TokenBatch, AttentionMaskBatch]]
+):
     def preprocess_example(self, example: TextRow) -> TokenTensor:
         return self.encode(example['text'])
     
