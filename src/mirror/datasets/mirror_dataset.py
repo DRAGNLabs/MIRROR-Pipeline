bd131a7dc7f54da7a43ad888cf1633636a97755d
Jansen Nye <jansen05@login02.rc.byu.edu>
2026-03-03 12:28:19 -0700

    Implement type args in mirror_dataset.py

--- src/mirror/datasets/mirror_dataset.py
+++ src/mirror/datasets/mirror_dataset.py
@@ -1,20 +1,19 @@
 from __future__ import annotations
-from typing import Callable, Sized, Sequence
+from typing import Callable, Sequence, Sized
 from torch.utils.data import Dataset
 from abc import abstractmethod
 from sys import stderr
 
-class MirrorDataset[RawT](Dataset[RawT], Sized):
+class MirrorDataset[RawT, ProcessedT](Dataset[RawT], Sized):
     
     @abstractmethod
     def __getitem__(self, index: int) -> RawT:
         pass
     
-    def preprocess[ProcessedT](self, preprocessor_function: Callable[[RawT], ProcessedT]) -> Sequence[ProcessedT]:
+    def preprocess(self, preprocessor_function: Callable[[RawT], ProcessedT]) -> Sequence[ProcessedT]:
         
-        def mappable_preprocessor_function(row):
-            row['input_ids'] = preprocessor_function(row)
-            return row
+        def mappable_preprocessor_function(row: RawT) -> dict[str, ProcessedT]:
+            return {"input_ids": preprocessor_function(row)}
 
         self.ds = self.ds.map(mappable_preprocessor_function)
         print("Tokenization complete.", file=stderr)
