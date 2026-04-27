d392081ae921ab975e4e1b83bde27dc82f0d2739
Jansen Nye <jansen05@login03.rc.byu.edu>
2026-04-27 10:43:27 -0600

    progress checkpoint

---
+++ src/mirror/datasets/fineweb_dataset.py
@@ -0,0 +1,65 @@
+from typing import Literal, cast
+
+import numpy as np
+from datasets import Dataset, DatasetDict
+
+from mirror.datasets.dataset_util import load_hf_dataset
+from mirror.datasets.mirror_dataset import MirrorDataset
+from mirror.row_types import TextRow
+
+hf_dataset_path = 'HuggingFaceFW/fineweb-edu'
+hf_dataset_name = 'sample-10BT'
+target_token_count = 2_000_000_000
+
+
+class FinewebDataset(MirrorDataset[TextRow]):
+    @property
+    def ds(self) -> Dataset:
+        return self._ds
+
+    def __init__(
+        self,
+        head: int | None = None,
+        skip: int | None = None,
+        split: Literal['train'] = 'train',
+    ):
+        """
+        Args:
+            head: how many examples to include. None includes the whole split.
+            skip: how many examples to skip from the start.
+            split: which dataset split to use.
+        """
+        super().__init__()
+
+        self._ds = cast(DatasetDict, load_hf_dataset(
+            hf_dataset_path,
+            hf_dataset_name,
+            self._process,
+        ))[split]
+
+        if skip:
+            self._ds = self._ds.select(range(skip, len(self._ds)))
+
+        if head:
+            self._ds = self._ds.select(range(head))
+
+    def _process(self, ds: DatasetDict | Dataset) -> DatasetDict | Dataset:
+        if isinstance(ds, DatasetDict):
+            return DatasetDict({split: self._truncate(d) for split, d in ds.items()})
+        return self._truncate(ds)
+
+    def _truncate(self, ds: Dataset) -> Dataset:
+        cumulative = np.cumsum(np.asarray(ds['token_count'], dtype=np.int64))
+        cutoff = int(np.searchsorted(cumulative, target_token_count, side='right')) + 1
+        if cutoff >= len(ds):
+            return ds
+        return ds.select(range(cutoff))
+
+    def to_row_type(self, ds_row: dict) -> TextRow:
+        return TextRow(text=ds_row['text'])
+
+    def __len__(self) -> int:
+        return len(self.ds)
+
+    def item(self, index) -> str:
+        return self.ds[index]['text']
