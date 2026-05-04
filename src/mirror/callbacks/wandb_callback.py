73f1a0d574ba9bbe8296cf03f79cdfb02a206241
Jansen Nye <jansen05@login01.rc.byu.edu>
2026-05-04 14:24:41 -0600

    Fix ticket

--- src/mirror/callbacks/wandb_callback.py
+++ src/mirror/callbacks/wandb_callback.py
@@ -19,13 +19,11 @@ class WandbCallback[RawT, ProcessedT, BatchT, ModelOutputT](
 ):
     def __init__(
         self,
-        extra_metrics_getter: ExtraMetricsGetter | None = None,
         log_every_n_steps: int = 1,
     ) -> None:
         super().__init__(is_singleton=True)
         self.run: WandbRun | None = None
         self.step = 0
-        self.extra_metrics_getter = extra_metrics_getter
         self.log_every_n_steps = log_every_n_steps
 
     def on_fit_start(
@@ -70,11 +68,6 @@ class WandbCallback[RawT, ProcessedT, BatchT, ModelOutputT](
         self.step += 1
         if self.step % self.log_every_n_steps != 0:
             return
-        extra_metrics = (
-            self.extra_metrics_getter.get_metrics(model, fabric)
-            if self.extra_metrics_getter
-            else {}
-        )
         if self.run:
             self.run.log({"train/loss": loss, **extra_metrics}, step=self.step)
 
