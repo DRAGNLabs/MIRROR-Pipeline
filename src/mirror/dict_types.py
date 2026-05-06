e58d1e64f75e5e810bc39332780ae59fc3bd6063
Mo Cheney <morrison.cheney@gmail.com>
2026-05-06 17:04:20 -0600

    Had to make this change because of circular import caused by Mirror Model being in the DictType

--- src/mirror/dict_types.py
+++ src/mirror/dict_types.py
@@ -1,6 +1,9 @@
-from typing import TypedDict
-from mirror.models.mirror_model import MirrorModel
+from __future__ import annotations
+from typing import TYPE_CHECKING, TypedDict
 from torch.optim import Optimizer
+
+if TYPE_CHECKING:
+    from mirror.models.mirror_model import MirrorModel
 
 class TextRow(TypedDict):
   text: str
